e254f3d810eae849cbcf2b109cd01f2c0e6cec35
Matthew Lai <matthew.lai@nix-digital.com>
2015-06-10 17:42:54 +0800

    Update cookie checking

--- middleware.js
+++ middleware.js
@@ -11,7 +11,7 @@ isNotNull = function(val) {
 }
 
 module.exports = function(options) {
-  var cookieName, cookieOptions, secret, sidName;
+  var cookieName, cookieOptions, secret, sidName, sessionId;
   sidName = options.sidName;
   secret = options.secret;
   cookieName = options.cookieName;
@@ -20,13 +20,17 @@ module.exports = function(options) {
     var cookies, original, signed;
     original = req.headers.cookie;
     foundCookie = isNotNull(original) && original.indexOf(cookieName) >= 0
-    if(foundCookie) {
+    if(!foundCookie) return next();
+    else {
       debug('original-header: %s', original);
       cookies = cookie.parse(original);
-      signed = 's:' + signature.sign(cookies[sidName], secret);
+      sessionId = cookies[sidName];
+      debug('sessionId: ' + sessionId);
+      if(!sessionId) return next();
+      signed = 's:' + signature.sign(sessionId, secret);
       req.headers.cookie = original + "; " + cookie.serialize(cookieName, signed, cookieOptions);
       debug('modified-header: %s', req.headers.cookie);
-    }
-    return next();
+      return next();
+    };
   };
 };
